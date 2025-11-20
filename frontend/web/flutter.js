a0aa81de0a1bbf6377659e197a68656be4184618
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-11-20 15:45:48 +0700

    Update flutter.js

--- frontend/web/flutter.js
+++ frontend/web/flutter.js
@@ -1,1 +1,19 @@
+// Flutter Web Loader (CDN version)
+if (!window._flutter) {
+  window._flutter = {};
+}
+
+_flutter.loader = {
+  loadEntrypoint: function (options) {
+    var script = document.createElement('script');
+    script.src = "https://storage.googleapis.com/flutter_infra_release/releases/stable/web/flutter.js";
+    script.defer = true;
+    script.onload = function () {
+      if (window._flutter) {
+        window._flutter.loader.loadEntrypoint(options);
+      }
+    };
+    document.body.appendChild(script);
+  }
+};
 
