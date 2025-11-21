ba43572d8cb6c13d498954d78790994468732f78
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-11-21 18:44:33 +0700

    Update supabase_config.dart

--- frontend/lib/supabase_config.dart
+++ frontend/lib/supabase_config.dart
@@ -1,1 +1,8 @@
+class SupabaseConfig {
+  // Thay bằng Project URL trong Supabase (API settings)
+  static const String SUPABASE_URL = "https://wfcjkunzpjmgjajxpkpv.supabase.co";
+
+  // Thay bằng anon public key trong Supabase (API settings)
+  static const String SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmY2prdW56cGptZ2phanhwa3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2NzM3MDgsImV4cCI6MjA3OTI0OTcwOH0.sWvtJlNZSR_lBGiHxuaf-1-9An0QZUWqZWlX2eNYH2I";
+}
 
