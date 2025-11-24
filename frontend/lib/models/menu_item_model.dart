5b79c339867f1030a7fdd243090889c8324da050
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-11-24 14:36:58 +0700

    Update menu_item_model.dart

--- frontend/lib/models/menu_item_model.dart
+++ frontend/lib/models/menu_item_model.dart
@@ -1,1 +1,12 @@
+class MenuItemModel {
+  final String title;
+  final String key;
+  final IconData icon;
+
+  MenuItemModel({
+    required this.title,
+    required this.key,
+    required this.icon,
+  });
+}
 
