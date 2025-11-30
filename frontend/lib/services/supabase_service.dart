acf09c3b077c6ab4785e1e25e93095ceb6a88912
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-11-30 13:42:07 +0700

    Update supabase_service.dart

--- frontend/lib/services/supabase_service.dart
+++ frontend/lib/services/supabase_service.dart
@@ -1,9 +1,5 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../config/supabase_config.dart';   // ⭐ THÊM IMPORT NÀY
 
 class SupabaseService {
-  static final SupabaseClient client = SupabaseClient(
-    SupabaseConfig.SUPABASE_URL,
-    SupabaseConfig.SUPABASE_ANON_KEY,
-  );
+  static SupabaseClient get client => Supabase.instance.client;
 }
